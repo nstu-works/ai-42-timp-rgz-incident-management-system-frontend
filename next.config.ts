b64d35151fee53eec6e97a2bd11032502397b5e7
lebowsskii <neepooha.1991@gmail.com>
2026-06-05 20:20:41 +0700

    add rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination: "http://awesome-pixel-api.duckdns.org/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
